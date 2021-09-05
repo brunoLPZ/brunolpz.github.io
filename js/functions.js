e7b013c5894750c632c56e8f7636342d9a1f45ca
Bruno Lopez <bruno.lopez.trigo@gmail.com>
2021-09-05 16:18:43 +0200

    Styling changes

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,27 @@
-function onAboutItemEnter($id) {
-  document.getElementById($id).classList.remove('hide');
-  document.getElementById($id).classList.add('animate-tooltip');
-}
+let currentIdx = 0;
+let topPosition = 0;
 
-function onAboutItemLeave($id) {
-  document.getElementById($id).classList.add('hide');
-  document.getElementById($id).classList.remove('animate-tooltip');
+function carousel() {
+  const interval = setInterval(() => {
+    const elements = document.getElementsByClassName('carousel-item');
+    if (currentIdx === elements.length) {
+      currentIdx = 0;
+      
+    }
+    for (let element of elements) {
+      const position = -currentIdx * 24;
+      element.animate([
+        // keyframes
+        { top: topPosition },
+        { top: `${position}rem` }
+      ], {
+        // timing options
+        duration: 2000,
+        fill: 'forwards'
+      });
+      topPosition = position;
+    }
+    currentIdx++;
+  }, 5000);
+
 }
